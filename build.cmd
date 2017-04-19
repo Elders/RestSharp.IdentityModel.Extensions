b562fd7146ada12a2ecfbd69babcf908462d96c0
SayliS <kalin.venkov@eldersoss.com>
2017-04-19 11:08:50 +0300

    Moves to proper file structure

---
+++ build.cmd
@@ -0,0 +1,30 @@
+@echo off
+
+SETLOCAL
+
+SET NUGET=%LocalAppData%\NuGet\NuGet.exe
+SET FAKE=%LocalAppData%\FAKE\tools\Fake.exe
+SET NYX=%LocalAppData%\Nyx\tools\build_next.fsx
+SET GITVERSION=%LocalAppData%\GitVersion.CommandLine\tools\GitVersion.exe
+
+echo Downloading NuGet.exe...
+IF NOT EXIST %NUGET% @powershell -NoProfile -ExecutionPolicy unrestricted -Command "New-Item -ItemType directory -Path %LocalAppData%\NuGet\; (New-Object System.Net.WebClient).DownloadFile('https://dist.nuget.org/win-x86-commandline/latest/nuget.exe','%NUGET%')"
+
+echo Downloading FAKE...
+IF NOT EXIST %LocalAppData%\FAKE %NUGET% "install" "FAKE" "-OutputDirectory" "%LocalAppData%" "-ExcludeVersion" "-Version" "4.50.0"
+
+echo Downloading GitVersion.CommandLine...
+IF NOT EXIST %LocalAppData%\GitVersion.CommandLine %NUGET% "install" "GitVersion.CommandLine" "-OutputDirectory" "%LocalAppData%" "-ExcludeVersion" "-Version" "3.6.1"
+
+echo Downloading Nyx...
+%NUGET% "install" "Nyx" "-OutputDirectory" "%LocalAppData%" "-ExcludeVersion"
+
+%FAKE% %NYX% "target=clean" -st
+%FAKE% %NYX% "target=RestoreNugetPackages" -st
+
+IF NOT [%1]==[] (set RELEASE_NUGETKEY="%1")
+
+SET SUMMARY="RestSharp.IdentityModel.Extensions"
+SET DESCRIPTION="RestSharp.IdentityModel.Extensions"
+
+%FAKE% %NYX% appName=RestSharp.IdentityModel.Extensions appSummary=%SUMMARY% appDescription=%DESCRIPTION% nugetPackageName=RestSharp.IdentityModel.Extensions nugetkey=%RELEASE_NUGETKEY%
